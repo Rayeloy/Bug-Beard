8399b97d688af9037e7176b72b046f188c7e2800
RAYELOY-PC\Eloy Jose <rayeloy@hotmail.com>
2017-10-14 00:24:03 +0200

    -Jump version 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     public bool accVersionOn;
     public bool accOnAir;
     public bool stopJumpOnCollision;
+    public bool jumpWithAcc;
+
     public float acceleration;
     public float deacceleration;
     public float MaxHorizontalSpeed;
@@ -24,7 +26,9 @@ public class PlayerMovement : MonoBehaviour
     private float HorzSpeed;
     private int deAccDir;
 
-    public float gravity;
+    public float gravityUp;
+    public float gravityDown;
+    private float gravityAct;
     public float jumpForce;
     private float distToGround;
     private bool jumping;
@@ -34,14 +38,33 @@ public class PlayerMovement : MonoBehaviour
     private float jumpingTime;
     public float maxTimeJumping;
 
+    public bool jumpWithHeight;
+    public bool shortHops;
+    public float maxHeight;//hay que darlo
+    public float timeToReach;//hay que darlo
+    private float gravity;//se calcula solo
+    private bool jumpingStopped;
+    private jumpphase phase;
+    public float gtimesStop;
+    private float initialHeight;
+    private float lastSpeedY;
+    private bool newOcurrence;
+
+
     public Transform groundcheck1;
     public Transform groundcheck2;
     //float groundRadius = 0.2f;
     public LayerMask whatIsGround;
     private bool IsGrounded;
-    
 
 
+    public enum jumpphase
+    {
+        none,
+        rise,
+        stop,
+        fall
+    };
 
 
 
@@ -52,46 +75,30 @@ public class PlayerMovement : MonoBehaviour
         HorzSpeed = 0;
         deAccDir = 0;
         jumping = false;
+        jumpingStopped = false;
+        phase = jumpphase.none;
+        gravity = (-2 * maxHeight) / Mathf.Pow(timeToReach, 2); Debug.Log("la gravedad es de " + gravity);
+        initialHeight = 0;
+        newOcurrence = true;
+    }
+    private void Start()
+    {
+        if (jumpWithHeight) jumpWithAcc = false;
+        if (accVersionOn) accOnAir = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         HorizontalMovement();
-        //Debug.Log("GRAVEDAD= " + IsGrounded + ", " + jumping);
-        if (!IsGrounded && !jumping)//------------------- LA GRAVEDAD NO PERDONA ---------------------------------
+        gravityFalls();
+        if (!jumpWithHeight)
         {
-
-            if (myRB.velocity.y > maxFallSpeed)
-            {
-                fallSpeed = Mathf.Clamp(myRB.velocity.y + (gravity * Time.deltaTime * -1), maxFallSpeed, maxJumpSpeed);
-            }
-            myRB.velocity = new Vector2(myRB.velocity.x, fallSpeed);
+            Jump();
         }
         else
         {
-            myRB.velocity = new Vector2(myRB.velocity.x, 0);
-        }
-        if (Input.GetButtonDown("Jump") && IsGrounded)//Nos abre las puertas para saltar
-        {
-            jumping = true;
-            jumpingTime = 0;
-            Debug.Log("comenzamos salto; jumping= " + jumping);
-        }
-        if (Input.GetButton("Jump") && jumping)//saltando
-        {
-            //contador de tiempo limite de manterner jump       
-            myRB.velocity = new Vector2(myRB.velocity.x, Mathf.Clamp(jumpForce * 1, maxFallSpeed, maxJumpSpeed));
-            jumpingTime += Time.deltaTime;
-            if (jumpingTime >= maxTimeJumping)
-                jumping = false;
-            Debug.Log("saltando...; jumping= " + jumping);
-        }
-        if (Input.GetButtonUp("Jump") && jumping)//no podemos seguir aumentando el salto al soltar el boton
-        {
-            jumping = false;
-            Debug.Log("salto terminado; jumping= " + jumping);
+            JumpWithHeight();
         }
 
     }
@@ -105,13 +112,12 @@ public class PlayerMovement : MonoBehaviour
             if (myRB.velocity.x < 0) spriteTransf.rotation = Quaternion.identity;
         }
 
-        if (accVersionOn || (accOnAir&&!IsGrounded))
+        if (accVersionOn || (accOnAir && !IsGrounded))
         {
             HorzSpeed = myRB.velocity.x;
             if (v != 0)//si hay input de movimiento
             {
 
-                Debug.Log("me muevo");
                 if (Mathf.Abs(HorzSpeed) < MaxHorizontalSpeed)
                 {
                     if (Mathf.Sign(HorzSpeed) != Mathf.Sign(v))
@@ -130,9 +136,8 @@ public class PlayerMovement : MonoBehaviour
                 HorzSpeed=MaxHorizontalSpeed;
             }*/
             }
-            else//no hay input
+            else if (myRB.velocity.x != 0)//no hay input
             {
-                Debug.Log("no me muevo");
                 if (HorzSpeed > 0)
                 {
                     HorzSpeed -= deacceleration * Time.deltaTime;
@@ -166,8 +171,126 @@ public class PlayerMovement : MonoBehaviour
             myRB.velocity = new Vector2(1 * HorzSpeed, myRB.velocity.y);
         }
         else
+            myRB.velocity = new Vector2(1 * MaxHorizontalSpeed * v, myRB.velocity.y);
+
+    }
+    void gravityFalls()
+    {
+        if (!IsGrounded)//------------------- LA GRAVEDAD NO PERDONA ---------------------------------
         {
-                myRB.velocity = new Vector2(1 * MaxHorizontalSpeed * v, myRB.velocity.y);
+            if (newOcurrence)
+            {
+                newOcurrence = false;
+                lastSpeedY = myRB.velocity.y;
+            }
+            if (jumpWithHeight)
+            {
+                switch (phase)
+                {
+                    case jumpphase.none:
+                        gravityAct = gravity;
+                        break;
+                    case jumpphase.rise:
+                        gravityAct = gravity;
+                        break;
+                    case jumpphase.stop:
+                        gravityAct = gravity * gtimesStop;
+                       // if((transform.position.y - initialHeight < maxHeight/4))
+                        break;
+                    case jumpphase.fall:
+                        gravityAct = gravity*gtimesStop/2;
+                        break;
+                }
+            }
+            else
+            {
+                if (myRB.velocity.y >= 0) gravityAct = gravityUp;
+                else gravityAct = gravityDown;
+            }
+
+            //CONTROLAMOS QUE NO SUPERE EL MAX DE CAIDA
+            if (myRB.velocity.y > maxFallSpeed)
+            {
+                fallSpeed = myRB.velocity.y + (gravityAct * Time.deltaTime);
+            }
+            else
+            {
+                fallSpeed = maxFallSpeed;
+            }
+            if(shortHops && phase==jumpphase.stop && (transform.position.y - initialHeight < maxHeight / 2))//CASO ESPECIAL PARA DAR SHORT HOPS
+            {
+                Debug.Log("CASO ESPECIAL");
+                fallSpeed = 0;
+                phase = jumpphase.fall;
+            }
+            fallSpeed = Mathf.Clamp(fallSpeed, maxFallSpeed, maxJumpSpeed);//poner valores grandes que no opriman la parabola
+            myRB.velocity = new Vector2(myRB.velocity.x, fallSpeed);
+        }
+    }
+    void Jump()
+    {
+        Debug.Log("isGrounded= " + IsGrounded);
+        if (Input.GetButtonDown("Jump") && IsGrounded)//Nos abre las puertas para saltar
+        {
+            jumping = true;
+            jumpingTime = 0;
+            Debug.Log("comenzamos salto; jumping= " + jumping);
+        }
+        if (Input.GetButton("Jump") && jumping)//saltando
+        {
+            //contador de tiempo limite de manterner jump  
+            Vector2 jumpingSpeed;
+            if (jumpWithAcc)
+            {
+                jumpingSpeed = new Vector2(myRB.velocity.x, Mathf.Clamp(jumpForce * 1 * Time.deltaTime, maxFallSpeed, maxJumpSpeed));
+            }
+            else
+            {
+                jumpingSpeed = new Vector2(myRB.velocity.x, maxJumpSpeed);
+            }
+            myRB.velocity = jumpingSpeed;
+            jumpingTime += Time.deltaTime;
+            if (jumpingTime >= maxTimeJumping)
+                jumping = false;
+            Debug.Log("saltando...; jumping= " + jumping);
+        }
+        if (Input.GetButtonUp("Jump") && jumping)//no podemos seguir aumentando el salto al soltar el boton
+        {
+            jumping = false;
+            Debug.Log("salto terminado; jumping= " + jumping);
+        }
+    }
+    void JumpWithHeight()
+    {
+
+        if (Input.GetButtonDown("Jump") && IsGrounded)//Nos abre las puertas para saltar
+        {
+            phase = jumpphase.rise;
+            jumpingTime = 0;
+            Debug.Log("comenzamos salto; jumping= " + phase.ToString());
+            initialHeight = transform.position.y;
+            myRB.velocity = new Vector2(myRB.velocity.x, 2 * maxHeight / timeToReach);
+        }
+        if (Input.GetButton("Jump") && phase == jumpphase.rise)//saltando
+        {
+            jumpingTime += Time.deltaTime;
+            if (jumpingTime >= timeToReach)
+                phase = jumpphase.fall;
+            Debug.Log("saltando...; jumping= " + phase.ToString());
+        }
+        if (Input.GetButtonUp("Jump") && phase == jumpphase.rise)//no podemos seguir aumentando el salto al soltar el boton
+        {
+            phase = jumpphase.stop;
+            Debug.Log("salto terminado; jumping= " + phase.ToString());
+        }
+        if (phase == jumpphase.stop && (myRB.velocity.y <= 0))
+        {
+            phase = jumpphase.fall;
+        }
+        if (phase == jumpphase.fall && IsGrounded)
+        {
+            phase = jumpphase.none;
+            newOcurrence = true;
         }
 
     }
@@ -181,9 +304,10 @@ public class PlayerMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D col)
     {
 
-        if(stopJumpOnCollision && jumping && (groundcheck2.position.y<=col.gameObject.transform.position.y + col.gameObject.GetComponent<BoxCollider2D>().bounds.extents.y))
+        if (stopJumpOnCollision && (jumping || phase == jumpphase.rise) && (groundcheck2.position.y <= col.gameObject.transform.position.y + col.gameObject.GetComponent<BoxCollider2D>().bounds.extents.y))
         {
             jumping = false;
+            phase = jumpphase.fall;
         }
     }
     private void OnCollisionExit2D(Collision2D col)
@@ -199,9 +323,9 @@ public class PlayerMovement : MonoBehaviour
         //distToGround = col.collider.bounds.extents.y;
         float top = (col.gameObject.transform.position.y + col.gameObject.GetComponent<BoxCollider2D>().bounds.extents.y);
         float feet = groundcheck1.position.y;//(transform.position.y - GetComponent<SpriteRenderer>().bounds.extents.y);
-        if (col.gameObject.tag == "ground" && top<=feet)
+        if (col.gameObject.tag == "ground" && top <= feet)
         {
-            IsGrounded = true;    
+            IsGrounded = true;
             //Debug.Log("Dist a ground= " + distToGround);
         }
         //Debug.Log("Grounded = " + IsGrounded+"; top= "+top+" y feet = "+feet);
